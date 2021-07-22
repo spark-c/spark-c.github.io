30389d07d4e7a8c468e3f13a11f5d90bd3f7efbe
Collin Sparks <cklsparks@gmail.com>
2021-07-22 11:36:16 -0500

    added project links, adjusted mobile projects styling

--- app/src/components/ProjectCard.js
+++ app/src/components/ProjectCard.js
@@ -6,11 +6,18 @@ const ProjectCard = ({ project, alignment, isLargeScreen }) => {
 
 
     return (
-        <div className={`project-card ${alignment}`}>
-            <img className="project-img" src={project["img-src"]}></img>
-            <div className="project-details">
-                <div className="project-title">{project["title"]}</div>
-                <div className="project-desc">{project["desc"]}</div>
+        <div className="project-card">
+            <div className={`project-info ${alignment}`}>
+                <img className="project-img" src={project["img-src"]}></img>
+                <div className="project-details">
+                    <div className="project-title">{project["title"]}</div>
+                    <div className="project-desc">{project["desc"]}</div>
+                </div>
+            </div>
+            <div className="project-links">
+                <a className="project-github" href={project["github-url"]}>GitHub</a>
+                {project["host-url"]?
+                <a className="project-host" href={project["host-url"]}>Live!</a>:undefined}
             </div>
         </div>
     );
--- app/src/components/Projects.js
+++ app/src/components/Projects.js
@@ -11,14 +11,14 @@ const Projects = ({ isLargeScreen }) => {
             "host-url": undefined,
             "github-url": "https://github.com/spark-c/baby-bot",
             "img-src": "https://via.placeholder.com/50.png",
-            "desc": "description"
+            "desc": "This is a longer description to test how things look when the descriptions start to get kinda long and require a lot of space to properly display on the screen."
         },
         "web-contact-converter": {
             "title": "web-contact-converter",
             "host-url": "web-contact-converter.herokuapp.com",
             "github-url": "https://github.com/spark-c/web-contact-converter",
             "img-src": "https://via.placeholder.com/50.png",
-            "desc": "description"
+            "desc": "This is a longer description to test how things look when the descriptions start to get kinda long and require a lot of space to properly display on the screen."
         },
         "coffee-calculator": {
             "title": "coffee-calculator",
