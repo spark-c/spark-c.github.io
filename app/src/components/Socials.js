f56850bb939b65a1f667113625a190bc710f8d86
Collin Sparks <cklsparks@gmail.com>
2021-07-23 01:44:43 -0500

    icon labels no longer show on mobile

--- app/src/components/Socials.js
+++ app/src/components/Socials.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 
 
 const Socials = () => {
+
+    const isLargeScreen = useMediaQuery('(min-width:450px)');
 
     return (
         <div className="socials-container">
@@ -9,19 +12,19 @@ const Socials = () => {
             <div className="socials-items">
                 <a href="https://github.com/spark-c">
                     <i className="fa fa-github"></i>
-                    <div className="social-label">
+                    <div className={isLargeScreen? "social-label":"hidden"}>
                         spark-c
                     </div>
                 </a>
                 <a href="https://twitter.com/collin_sparkles">
                     <i className="fa fa-twitter"></i>
-                    <div className="social-label">
+                    <div className={isLargeScreen? "social-label":"hidden"}>
                         @collin_sparkles
                     </div>
                 </a>
                 <a href="https://linkedin.com/in/collin-sparks">
                     <i className="fa fa-linkedin"></i>
-                    <div className="social-label">
+                    <div className={isLargeScreen? "social-label":"hidden"}>
                         Collin Sparks
                     </div>
                 </a>
