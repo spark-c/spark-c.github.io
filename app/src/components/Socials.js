155687c9c70dc2fdcb5074e1567424914776594b
Collin Sparks <cklsparks@gmail.com>
2021-07-23 01:14:00 -0500

    style updates to socials, header, contact submit

--- app/src/components/Socials.js
+++ app/src/components/Socials.js
@@ -8,13 +8,22 @@ const Socials = () => {
             <div className="socials-title">Socials</div>
             <div className="socials-items">
                 <a href="https://github.com/spark-c">
-                    <i className="icon github">GitHub</i>
+                    <i className="fa fa-github"></i>
+                    <div className="social-label">
+                        spark-c
+                    </div>
                 </a>
                 <a href="https://twitter.com/collin_sparkles">
-                    <i className="icon twitter">Twitter</i>
+                    <i className="fa fa-twitter"></i>
+                    <div className="social-label">
+                        @collin_sparkles
+                    </div>
                 </a>
                 <a href="https://linkedin.com/in/collin-sparks">
-                    <i className="icon linkedin">LinkedIn</i>
+                    <i className="fa fa-linkedin"></i>
+                    <div className="social-label">
+                        Collin Sparks
+                    </div>
                 </a>
             </div>
         </div>
