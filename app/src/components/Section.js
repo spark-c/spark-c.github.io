0f7fcbaecd35321c8cf3eeca09ac3478dc55f1da
Collin Sparks <cklsparks@gmail.com>
2021-07-23 11:57:29 -0500

    added socials to bottom of other sections

--- app/src/components/Section.js
+++ app/src/components/Section.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
+import Socials from './Socials';
+
 
 const Section = ({ children }) => {
+
+    const isContact = children.type.name === "Contact"? true:false;
 
     return (
         <div className="section-container">
             {children}
+            {isContact? undefined:<Socials />}
         </div>
     );
 };
