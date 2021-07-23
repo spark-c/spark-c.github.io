0364f28584e23931cf47903d8011814842438fb3
Collin Sparks <cklsparks@gmail.com>
2021-07-23 01:38:45 -0500

    adjustments to desktop ProjectCards

--- app/src/components/ProjectCard.js
+++ app/src/components/ProjectCard.js
@@ -6,20 +6,26 @@ const ProjectCard = ({ project, alignment, isLargeScreen }) => {
 
 
     return (
-        <div className="project-card">
-            <div className={`project-info ${alignment}`}>
-                <img className="project-img" src={project["img-src"]}></img>
+        <div className={`project-card  ${alignment}`}>
+            <img className="project-img" src={project["img-src"]}></img>
+            <div className="project-info">
                 <div className="project-details">
-                    <div className="project-title item-margin">{project["title"]}</div>
-                    <div className="project-stack item-margin">{project["stack"]}</div>
-                    <div className="project-desc item-margin">{project["desc"]}</div>
+                    <div className="project-title item-margin">
+                        {project["title"]}
+                    </div>
+                    <div className="project-stack item-margin">
+                        {project["stack"]}
+                    </div>
+                    <div className="project-desc item-margin">
+                        {project["desc"]}
+                    </div>
                 </div>
-            </div>
-            <div className="project-links">
-                <a className="project-github" href={project["github-url"]}>GitHub</a>
-                {project["host-url"]?
-                <a className="project-host" href={project["host-url"]}>Live!</a>:undefined}
-            </div>
+                <div className="project-links">
+                    <a className="project-github" href={project["github-url"]}>GitHub</a>
+                    {project["host-url"]?
+                    <a className="project-host" href={project["host-url"]}>Live!</a>:undefined}
+                </div>
+                </div>
         </div>
     );
 };
