00a2e761a30e64eedb0267d5035f502f0d25c5f4
Collin Sparks <cklsparks@gmail.com>
2021-07-26 21:03:38 -0500

    completed req/resp to/from hashnode api

--- app/src/components/Blog.js
+++ app/src/components/Blog.js
@@ -1,7 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
 const Blog = () => {
+
+    const [posts, setPosts] = useState(undefined);
+    
+    const query = `
+    {
+        user(username: "collin-sparkles") {
+          publication {
+            posts(page: 0) {
+              title
+              brief
+              slug
+              dateAdded
+              coverImage
+            }
+          }
+        }
+      }
+    `
+
+    const sendRequest = (e) => {
+        e.preventDefault();
+
+        async function handleResponse() {
+            const data = await fetch('https://api.hashnode.com/', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    query:query,
+                    variables:{}
+                })
+            });
+
+            return data.json();
+        }
+
+        async function getPosts() {
+            const handled = await handleResponse();
+            setPosts(handled.data.user.publication.posts)
+        }
+        getPosts();
+    };
+
+    const renderedPosts = posts?
+        posts.map(post => {
+            return(
+                <div key={post.title}>{post.title}</div>
+            );
+        }):
+        undefined
 
     return (
         <div className="section blog">
@@ -10,6 +61,10 @@ const Blog = () => {
                 <p>
                     This is blog content!
                 </p>
+                <button onClick={sendRequest} value="REQUEST">REQUEST</button>
+                <div>
+                    {renderedPosts}
+                </div>
             </div>
         </div>
     );
