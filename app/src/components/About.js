4d0a9125d8874d80760ce7a3263a20509d1b5347
Collin Sparks <cklsparks@gmail.com>
2021-07-31 19:31:55 -0500

    added grievous easter egg

--- app/src/components/About.js
+++ app/src/components/About.js
@@ -1,32 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import grievous from '../images/grievous.png';
 
 
 const About = () => {
+    const [showImg, setShowImg] = useState(false);
 
     return (
         <div className="section about">
-            <h1 className="section-title">_ Hello there! :) _</h1>
+            <h1 className="section-title"
+            onMouseEnter={() => setShowImg(true)}
+            onMouseLeave={() => setShowImg(false)}
+            onClick={() => setShowImg(!showImg)}>
+                Hello there!
+            </h1>
+            {
+                showImg?
+                <img
+                    className="grievous"
+                    src={grievous}
+                    alt="Star Wars' General Grievous saying 'General Kenobi!"
+                />:
+                undefined
+            }
             <div className="about-content">
                 <p>
                     Hiya! I am having a great time working with Python and JavaScript and whatever other techs I come across end up needing to learn in order to do my thing.
                 </p>
-                
-                <p>A decade of ballet taught me:</p>
-
-                <ol>
-                    <li>Persistence and discipline are the foundation of improvement.</li>
-                    <li>Beauty comes from doing something so many times that it becomes an art.</li>
-                    <li>When you study an art, you recognize beauty hidden in everything.</li>
-                    <li>Every thing and every skill can be an art.</li>
-                </ol>
-
-                <p>When someone performs their art, I want to understand it. I seek these new ideas at every turn, so that I can appreciate their beauty -- That beauty inspires learning.</p>
-
-                <p>Later, I will remember my new skill -- that person's art -- and use it to make my own work better.</p>
-
-                <p>More informed. More effective. More efficient. More beautiful.</p>
-
-                <p>Your art is fascinating... tell me about it!</p>
                 
             </div>
         </div>
