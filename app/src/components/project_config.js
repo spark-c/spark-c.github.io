c10f8ca7aed18cbc7a1d24846f183544c3e3307c
Collin Sparks <cklsparks@gmail.com>
2021-08-02 12:14:27 -0500

    moved project info to project_config.js

---
+++ app/src/components/project_config.js
@@ -0,0 +1,63 @@
+import * as React from 'react';
+import WidgetBot from '@widgetbot/react-embed';
+
+import wcc_img from '../images/wcc.png';
+import sparksiebot_img from '../images/sparksiebot.png';
+
+
+const project_details = {
+    'baby-bot': {
+        "title": "SparksieBOT",
+        "host-url": true,
+        "github-url": "https://github.com/spark-c/SparksieBOT",
+        "img-src": sparksiebot_img,
+        "desc": (
+            <div>
+                <p>This bot is created and maintained to provide commands for utility and entertainment on my personal Discord server!</p>
+                <p>The project goes varying periods of light maintainance, heavy development for additional functionality, and occasional upkeep work to refactor old code to meet my improving code-quality standards.</p>
+                <p className="cta">Click the "Live!" link to say hi and interact with SparksieBot!</p>
+            </div>
+        ),
+        "stack": "Python (discord.py, SQLAlchemy) | Postgres | Heroku",
+        "cta": "Live!",
+        "embed": (
+            <WidgetBot
+                className="widgetbot"
+                server="742418152280948857" // sparksie test server
+                channel="871196944754696233" // sparksiebot-live-demo
+            />
+        ),
+    },
+    "web-contact-converter": {
+        "title": "web-contact-converter",
+        "host-url": "http://web-contact-converter.herokuapp.com",
+        "github-url": "https://github.com/spark-c/web-contact-converter",
+        "img-src": wcc_img,
+        "desc": (
+            <div>
+                <p>A local company had a frequent task of manually transferring sales leads/contact information from a plain-text document into a spreadsheet -- which took <span className="italic">hours</span> at a time. This app is used to enormously expedite the process!</p>
+                <p>Note: The app is served from a Heroku "free" dyno, and may take several seconds to load while it wakes up!</p>
+            </div>
+        ),
+        "stack": "Python (Flask, SQLAlchemy) | JavaScript | Postgres | Heroku",
+        "cta": "Live!",
+        "embed": undefined,
+    },
+    "coffee-calculator": {
+        "title": "coffee-calculator",
+        "host-url": undefined,
+        "github-url": "https://github.com/spark-c/coffee-calculator",
+        "img-src": "https://via.placeholder.com/50.png",
+        "desc": (
+            <div>
+                <p>A small web app used for calculating the necessary variables of a pour-over coffee recipe.</p>
+                <p>There are several modes to account for the various common combinations of given/desired values to calculate. Soon including a setting for making iced coffee! Based on V60 paramters.</p>
+            </div>
+        ),
+        "stack": "JavaScript (React)",
+        "cta": "Live!",
+        "embed": undefined,
+    }
+}
+
+export default project_details;
