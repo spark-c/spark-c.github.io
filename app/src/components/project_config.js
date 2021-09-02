7862fd3369142c3e4af9ce7b5c08e6ea4cd67051
Collin Sparks <cklsparks@gmail.com>
2021-09-01 19:51:33 -0500

    nitpicked project descriptions

--- app/src/components/project_config.js
+++ app/src/components/project_config.js
@@ -17,7 +17,7 @@ const project_details = {
             <div>
                 <p>This bot is created and maintained to provide commands for utility and entertainment on my personal Discord server!</p>
                 <p>The project goes varying periods of light maintainance, heavy development for additional functionality, and occasional upkeep work to refactor old code to meet my improving code-quality standards.</p>
-                <p>You can click the "Live!" link to interact with SparksieBOT; commands found in the GitHub Readme (TODO: put them here!)</p>
+                <p>You can click the "Live!" link to interact with SparksieBOT; commands found in the GitHub Readme (Coming soon: !demo command)</p>
             </div>
         ),
         "stack": "Python (discord.py, SQLAlchemy) | Postgres | Heroku",
@@ -39,7 +39,7 @@ const project_details = {
         "desc": (
             <div>
                 <p>A local company had a frequent task of manually transferring sales leads/contact information from a plain-text document into a spreadsheet -- which took <span className="italic">hours</span> at a time. This app is used to enormously expedite the process!</p>
-                <p>Note: The app is served from a Heroku "free" dyno, and may take several seconds to load while it wakes up!</p>
+                <p>Note: The app is served from a Heroku "free" dyno, and unfortunately may take several seconds to load while it wakes up!</p>
             </div>
         ),
         "stack": "Python (Flask, SQLAlchemy) | JavaScript | Postgres | Heroku",
